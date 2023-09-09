ec06ca4885b592f9f8b7349294416a3e81c74eca
fp40 <luisfelipebianchini@gmail.com>
2023-09-09 15:09:03 -0300

    deploy web and db

--- api/db.js
+++ api/db.js
@@ -1,9 +1,13 @@
-import mysql from "mysql"
-const db = mysql.createConnection({
-    host: "localhost",
-    user: 'root',
-    password: "",
-    database: 'crud'
-})
+import dotenv from "dotenv";
 
-export default db
+dotenv.config({ path: ".env.local" });
+
+import mysql from "mysql";
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+});
+
+export default db;
